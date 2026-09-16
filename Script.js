9c5740068d34b506b2bc0d1eec0b5716ec8c4f8d
safala444 <safalamaharjan444@gmail.com>
2026-09-16 22:44:26 +0545

    script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    document.getElementById("navLinks").classList.toggle("show");
-}
-
-document.querySelectorAll("#navLinks a").forEach(function(link) {
-    link.addEventListener("click", function() {
-        document.getElementById("navLinks").classList.remove("show");
-    });
-});
